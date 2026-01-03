bb0f372e1b3c3bf6ecaa33af91890533a7604f4a
Abdulkabir fagbohun <abdulkabirfagbohun@gmail.com>
2026-01-03 08:49:47 +0100

    Add config.js for production use

---
+++ config.js
@@ -0,0 +1,6 @@
+const AppConfig = {
+    GOOGLE_MAPS_API_KEY: "AIzaSyAUqbDPvfPNZAjQFD50PlnYPRhIcNGABEE", // Replace with your actual Google Maps API key
+    // API_BASE_URL: "https://localhost:7288", // Use "https://your-production-api.com" for production
+    API_BASE_URL: "https://emergencyrescuesystem.onrender.com", // Use "https://your-production-api.com" for production
+    GOOGLE_CLIENT_ID: "88984039780-g17q7897o6fsec1oimirdbjv3otoor2e.apps.googleusercontent.com" // Replace with your actual Google Client ID
+};
